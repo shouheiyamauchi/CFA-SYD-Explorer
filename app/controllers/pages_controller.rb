8f35d36ffeecb507f171ead3bdc1dc7eefee6830
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-04-22 23:47:50 +1000

    add save grid functionality to root page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   before_action :enable_header, :enable_navbar
 
   def home
+    @dashboard_items = ["partials/dashboard/map", "partials/dashboard/events", "partials/dashboard/calendar", "partials/dashboard/event_attendance_history", "partials/dashboard/children"]
+    @dashboard_grid = JSON.parse current_user.dashboard_grid
   end
 
   def test3
@@ -15,6 +17,6 @@ class PagesController < ApplicationController
 
   def save_grid
     current_user.update_attribute :dashboard_grid, params[:dashboard_grid]
-    redirect_to pages_test4_path
+    redirect_to root_path
   end
 end
