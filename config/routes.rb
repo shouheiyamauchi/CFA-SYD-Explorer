e4712b384d9303053e429a271816f472e1168a74
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-04-25 18:52:56 +1000

    update map code to be dynamic

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,8 @@ Rails.application.routes.draw do
   get 'signup/admin'
   post 'signup/create_user'
 
+  get 'pages/test'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
